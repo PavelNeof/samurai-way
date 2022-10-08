d297395dc88ea5462c1e8e69ef4d0c9ea7fee2de
PavelNeof <pavelneof@gmail.com>
2022-10-08 14:45:11 +0300

    85 lessons commit

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import {AppStateType} from "../../../redux/redux-store";
 
 type ProfileStatusPropsType = {
@@ -10,6 +10,10 @@ export const ProfileStatusWithHooks = (props: ProfileStatusPropsType) => {
 
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    },[props.status])
 
     const activateEditMode = () => {
         setEditMode(true)
